bf361704e0e1d1a8323a4cb6f0cfdfa653bddd90
dhieu <hieudoanmanh.it@gmail.com>
2022-05-13 12:36:29 +0700

    update

--- src/main/java/org/dhieu/rw/RenderJsonTemplate.java
+++ src/main/java/org/dhieu/rw/RenderJsonTemplate.java
@@ -7,16 +7,13 @@ import java.io.InputStream;
 
 public class RenderJsonTemplate {
 
-
     public static void main(String[] args) {
-
         try {
             IRenderWord renderWord = new RenderWord();
             ClassLoader classloader = Thread.currentThread().getContextClassLoader();
             InputStream inputStream = classloader.getResourceAsStream("demo.json");
-            JSONObject jsonObject = null;
             assert inputStream != null;
-            jsonObject = new JSONObject(IOUtils.toString(inputStream));
+            JSONObject jsonObject = new JSONObject(IOUtils.toString(inputStream));
             renderWord.render(jsonObject, "demo.docx", "simple.docx");
         } catch (Exception e) {
             System.out.println("ERROR");
